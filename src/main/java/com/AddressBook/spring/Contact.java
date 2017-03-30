251f625d24dc3124c719d026e43c21ad895e251a
dzksnpk <dzekson.dzekson@gmail.com>
2017-03-30 13:43:57 +0200

    Update Contact.java

--- src/main/java/com/AddressBook/spring/Contact.java
+++ src/main/java/com/AddressBook/spring/Contact.java
@@ -12,8 +12,8 @@ public class Contact {
 	
 	
 	@Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    	@GeneratedValue(strategy = GenerationType.AUTO)
+    	private long id;
 	
 	private String name;
 	private String surname;
